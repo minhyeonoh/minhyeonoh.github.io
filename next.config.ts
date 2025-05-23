389606adfce5a43a060c3aa5856b7f5b7c4173d1
Minhyeon Oh <minhyeon.oh@postech.ac.kr>
2025-05-23 14:06:29 +0900

    MDX

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
-
+import remarkGfm from 'remark-gfm'
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+import createMDX from '@next/mdx'
+ 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+}
+ 
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: [
+      remarkGfm,
+      remarkMath,
+    ],
+    rehypePlugins: [
+      rehypeKatex,
+    ],
+  },
+})
+ 
+export default withMDX(nextConfig)
